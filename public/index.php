30957d1bc46aca070db8c7c5ac3570da51bf63d8
Hilton Sousa <hilton.sousa@gmail.com>
2026-04-10 10:50:44 -0300

    Explicit errors

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Carregar bootstrap (fora da pasta public)
 require_once dirname(__DIR__) . '/app/bootstrap.php';
 
